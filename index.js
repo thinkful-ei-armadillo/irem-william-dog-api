f8370d1915d16871e72336194e29d3fec4cc659a
William Y Wong <wwong1987@gmail.com>
2019-01-25 11:25:11 -0800

    finised up assignment 1 & 2

--- index.js
+++ index.js
@@ -4,19 +4,33 @@
 $('form').on('submit', function(e) {
   e.preventDefault();
   let numberOfDogs = $('.userNumber').val();
-  getDogImage(numberOfDogs)
-    .then(response => response.json())
-    .then(jsonData => dogImageDisplay(jsonData));
+  console.log(numberOfDogs);
+  if (numberOfDogs >= 1 && numberOfDogs <= 50) {
+    getDogImage(numberOfDogs)
+      .then(response => response.json())
+      .then(jsonData => dogImageDisplay(jsonData));
+  } else {
+    throw new Error ('Number must be between 1 and 50');
+  }
 });
 
 function getDogImage(number) {
   return fetch(`https://dog.ceo/api/breeds/image/random/${number}`);
 }
 
-function dogImageDisplay(response){
+function dogImageDisplay(response) {
   let imageElements= [];
-  for(let i=0; i<response.message.length; i++){
+  for (let i=0; i<response.message.length; i++){
     imageElements.push(`<img class="results-img" src="${response.message[i]}">`);
   }
   $('.js-display-images').html(imageElements);
 }
+
+
+
+
+
+
+
+
+
