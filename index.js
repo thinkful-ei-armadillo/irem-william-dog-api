0a386c4c3d138205bcb159dd8cfb6177e1c829aa
William Y Wong <wwong1987@gmail.com>
2019-01-25 12:07:03 -0800

    randomize breed button works

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 /*global $*/
 
 'use strict';
-$('form').on('submit', function(e) {
-  e.preventDefault();
-  let numberOfDogs = $('.userNumber').val();
-  console.log(numberOfDogs);
-  if (numberOfDogs >= 1 && numberOfDogs <= 50) {
-    getDogImage(numberOfDogs)
-      .then(response => response.json())
-      .then(jsonData => dogImageDisplay(jsonData));
-  } else {
-    throw new Error ('Number must be between 1 and 50');
-  }
-});
+function handlingDogMultple() {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    let numberOfDogs = $('.userNumber').val();
+    console.log(numberOfDogs);
+    if (numberOfDogs >= 1 && numberOfDogs <= 50) {
+      getDogImage(numberOfDogs)
+        .then(response => response.json())
+        .then(jsonData => dogImageDisplay(jsonData));
+    } else {
+      throw new Error ('Number must be between 1 and 50');
+    }
+  });
+}
 
 function getDogImage(number) {
   return fetch(`https://dog.ceo/api/breeds/image/random/${number}`);
@@ -27,10 +29,32 @@ function dogImageDisplay(response) {
 }
 
 
+function getRandomBreedImage() {
+  return fetch('https://dog.ceo/api/breed/hound/images/random')
+    .then(response => response.json())
+    .then(responseJson => dogDisplayRandomBreed(responseJson));
+}
 
 
+function dogDisplayRandomBreed(responseJson) {
+  let randomImage = `<img class="results-img" src="${responseJson.message}">`;
+  $('.js-display-images').html(randomImage);
+}
+
+function handleRandomBreed() {
+  $('form').on('click', '#randomizeButton', function(e) {
+    e.preventDefault();
+    getRandomBreedImage();
+    dogDisplayRandomBreed();
+  }
+  );
+}
 
 
+function main() {
+  handleRandomBreed();
+  handlingDogMultple();
+}
 
 
-
+$(main);
